399709a294139608fe9e07238773f517fd87a2bd
remnasa <remnasa@proton.me>
2025-01-02 18:34:38 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,26 @@
+# URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+# Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+# Input json: { "raw_document": { "text": text_to_analyse } }
+exit
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    """ This fucntion defines the URL for emotion analysis API,
+    Creates the payload with the text to be analyzed, sets the headers, 
+    makes the post request to the API with the payload and headers, 
+    parses the response from the API, and provides the status code.
+    """
+    # Define the URL for the emotion analuysis API
+    url = """https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"""
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    return response.text
