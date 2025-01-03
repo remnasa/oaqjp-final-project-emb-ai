58accf14e70f4fc09c71e3e37d7cf45b675c0679
remnasa <remnasa@proton.me>
2025-01-02 21:03:30 -0500

    Corrected phrase errors

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
@@ -9,19 +9,29 @@ def emot_detector():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the emotion_detector function and store the response
-    response = emotion_detector(text_to_analyze)
+    if not text_to_analyze:
+        return jsonify({"error": "Missing ;textToAnalyze' query parameter"}), 400
+    
+    try:
 
-    # Extract the label and score from the response
-    all_emotions = response['emotion_scores']
-    dom_emotion = response['dominant_emotion']
+        # Pass the text to the emotion_detector function and store the response
+        response = emotion_detector(text_to_analyze)
 
-    # Return the detected emotions along with the dominant one
-    return f"For the given statement, the system response is {all_emotions}. The dominant emotion is {dom_emotion}."
+        all_emotions = {key: response[key] for key in response if key != 'dominant_emotion'}
+        dom_emotion = response["dominant_emotion"]
+
+        # Return the detected emotions along with the dominant one
+        phrase = f"For the given statement, the system response is {all_emotions}. The dominant emotion is {dom_emotion}."
+    
+        return phrase
+
+    except Exception as e:
+
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
