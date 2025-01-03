7363cca3e8b388968dedc812e1b6f48539fb9b1e
remnasa <remnasa@proton.me>
2025-01-02 20:27:31 -0500

    App development

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emot_detector():
+    
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the label and score from the response
+    all_emotions = response[emotion_scores]
+    dom_emotion = response['dominant_emotion']
+
+    # Return the detected emotions along with the dominant one
+    return f"For the given statement, the system response is {all_emotions}. The dominant emotion is {dom_emotion}."
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
